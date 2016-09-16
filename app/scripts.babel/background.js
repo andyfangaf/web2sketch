e24ca87748395e8c80115489e0807b30f8597ee8
Andy Fang <andy.fang@shopify.com>
2016-09-16 08:58:38 -0400

    crop hopefully working

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -6,8 +6,8 @@ chrome.browserAction.setBadgeText({text: 'Make'});
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request == 'screenshot') {
-    chrome.tabs.captureVisibleTab(null, {quality: 50, format: 'png'}, function(image) {
-      console.log(image);
+    chrome.tabs.captureVisibleTab(null, {quality: 100, format: 'png'}, function(image) {
+      console.log('captured image');
       sendResponse(image);
     });
   }
--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -10,5 +10,42 @@ document.body.addEventListener('click', function(evt) {
   console.log('clicked');
   chrome.runtime.sendMessage('screenshot', function(response) {
     console.log(response);
+    createImage(response)
   })
 })
+
+function appendToBody(dataURL) {
+  const imageTag = document.createElement('img')
+  imageTag.setAttribute('src', dataURL)
+  document.body.appendChild(imageTag)
+}
+
+function createImage(dataURL) {
+  const canvas = createCanvas(window.innerWidth, window.innerHeight)
+  const context = canvas.getContext('2d')
+  const croppedImage = new Image()
+
+  croppedImage.onload = function() {
+    // parameter 1: source image (screenshot)
+    // parameter 2: source image x coordinate
+    // parameter 3: source image y coordinate
+    // parameter 4: source image width
+    // parameter 5: source image height
+    // parameter 6: destination x coordinate
+    // parameter 7: destination y coordinate
+    // parameter 8: destination width
+    // parameter 9: destination height
+
+    context.drawImage(croppedImage, 10, 10, 300, 300, 0, 0, 250, 250)
+    appendToBody(canvas.toDataURL())
+  }
+}
+
+function createCanvas(canvasWidth, canvasHeight) {
+    var canvas = document.createElement("canvas");
+
+    // size of canvas in pixels
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    return canvas;
+}
